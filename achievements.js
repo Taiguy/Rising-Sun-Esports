c26389cade521c1defcb9da762747ec5aaa27c85
Shota <shota1wa@yahoo.co.jp>
2020-07-20 22:47:25 -0700

    Fixed collapseNum bug in achievements

--- achievements.js
+++ achievements.js
@@ -6,7 +6,7 @@ var achievements = [
         rank:"Top 16",
         descriptionEN:"316 teams competed in the July Qualifiers for the Brawl Stars Championship. KIX Rising Sun beat ブロスタUOEH, First Base, and Chick-bear-grinning to finish the first day of the tournament without losing a single set. However, KIX Rising Sun lost to QWERTY in the Round of 16. ",
         descriptionJP:"７月の Brawl Stars Championship予選には３１６チームが参戦しました。KIX Rising Sun は１回戦と２回戦シード権を与えられ、初日はブロスタUOEH、First Base、Chick-bear-grinningに勝利しました。続く２日目はQWERTYに敗れ、トップ１６でトーナメントを終えました。",
-        collapseNum:"one"
+        collapseNum:"nine"
     },
     {
         game:"BS",
@@ -15,7 +15,7 @@ var achievements = [
         rank:"4th",
         descriptionEN:"KIX Rising Sun, formerly known as きまぐれ P.R.O, competed in the June Qualifiers for the Brawl Stars Championship. 361 teams competed in the Qualifiers tournament, and KIX Rising Sun had a first-round bye. The team won comfortably, beating Panna Cotta, PSG Esports, Duy Anh Esport, IGC, Cloud Gaming, and Fire Scale Esports. In the semi-finals, KIX Rising Sun was defeated by 君が名はB-Boy指定, and for the third place match, SK Miracle took the win, resulting in a 4th place finish.",
         descriptionJP:"きまぐれP.R.O として活動していたKIX Rising Sun が６月の Brawl Stars Championship 予選に参戦しました。合計361チームがトーナメントに出場し、KIX Rising Sun は１回戦シード権を与えられました。Panna Cotta, PSG Esports, Duy Anh Esport, IGC, Cloud Gaming, Fire Scale Esports に勝利し、準決勝で君の名はB-Boy指定に敗れました。３位決定戦は SK Miraclesに惜しくも負けてしまい、結果は４位となりました。",
-        collapseNum:"one"
+        collapseNum:"eight"
     },
     {
         game:"CR",
@@ -24,7 +24,7 @@ var achievements = [
         rank:"1st",
         descriptionEN:"Rising Sun Esports split into two teams - Alpha and Beta. 27 teams were split into 3 groups, where Alpha was placed into Group B and Beta was placed into Group C. Unlike other leagues, Clash Cup had a point-based ranking system. Alpha team finished at the top of their group scoring 74 points. 71 points by the Beta team was enough to secure them the 3rd place spot in their group and both teams advanced to the playoffs. Alpha received a first-round bye for their performance, and Beta was knocked out in the first round by Instinct NA. Alpha took revenge on Instinct in the quarter-finals and advanced to the semi-finals. Alpha team barely managed to get the victory over Evolve and faced Team TyrNE in the finals. In the finals, Rising Sun Esports lost to Team TyrNE, but later it was discovered Team TyrNE had violated the league rules so Rising Sun Esports was given the title.",
         descriptionJP:"Clash Cup Season 4には、Rising Sun Esportsをalphaとbetaの2つのチームに分かれ挑戦しました。世界各国から集まった合計27チームは3つのグループに分けられ、alphaはグループBに、betaはグループCに割り振られました。alphaチームは74点を獲得してグループBを一位通過し、betaチームは71点でグループ内3位通過で、両チームともにプレーオフに進出しました。alphaはシードで1回戦免除、betaは1回戦でInstinct NAに負けてしまいました。続く準々決勝で、alphaはInstinct NAにリベンジを果たし、準決勝に進みました。次の試合で、AlphaチームはEvolveに辛うじて勝利を収め、決勝戦ではTeam TyrNEと対戦。決勝戦では、Rising Sun Esports AlphaはTeam TyrNEに敗れましたが、後にTeam TyrNEのルール違反が発覚し、Rising Sun Esportsにタイトルが与えられました。",
-        collapseNum:"two"
+        collapseNum:"seven"
     },
     {
         game:"CR",
@@ -33,7 +33,7 @@ var achievements = [
         rank:"2nd",
         descriptionEN:"Representing Japan, Rising Sun Esports competed in a nation-based league for APAC. 8 countries were represented, and each team played against each other. Rising Sun Esports finished the regular season with a 4-3 record, placing 3rd. For the playoffs, the top 4 teams were placed in the top bracket, while the remaining 4 teams were placed in the bottom bracket. Top bracket was a double-elimination while the bottom bracket was single-elimination. Rising Sun Esports had a tough start to the playoffs getting defeated by India 3-1 and was sent down to the bottom bracket. From there, Rising Sun Esports did not give up, defeating South Korea, Malaysia, and Taiwan, reaching the grand finals. Seeking revenge from the first round, Rising Sun Esports faced off against India again in the finals, but was defeated once again and finished the season 2nd place. ",
         descriptionJP:"日本代表としてAsia City Leagueに出場したRising Sun Esportsは、APAC値域の国別リーグに出場しました。合計8カ国が参加し、各チームがお互いと対戦しました。レギュラーシーズンでは4勝3敗で、3位で通過しました。プレーオフでは、上位4チームがトップブラケットに、残りの4チームがボトムブラケットに割り振られました。Rising Sun Esportsは、インドに1-3で敗れましたが、そこからも諦めずに韓国、マレーシア、台湾を撃破し、なんとか決勝に駒を進めました。プレイオフ初戦のリベンジを果たそうとしましたが、決勝戦では惜しくも敗れ、2位でシーズンを終えました。",
-        collapseNum:"three"
+        collapseNum:"six"
     },    
     {
         game:"CR",
@@ -42,7 +42,7 @@ var achievements = [
         rank:"3rd",
         descriptionEN:"The second season of Neo Clash League featured 16 teams, split into two different groups. Rising Sun Esports split into teams - Alpha and Beta. This was the first time Rising Sun Esports split into two different teams. Alpha team finished 2nd in Group A at 5-2, and Beta team finished at the top of Group B at 6-1. Top 4 teams from each group advanced to the playoffs. Both Alpha and Beta won the first round, but lost in the semi-finals.",
         descriptionJP:"Neo Clash League Season 2は、16チームが2つのグループに分けられて開催されました。Rising Sun Esportsはalphaとbetaの2チームに分かれて挑戦しました。alphaはグループAを5-2で2位通過、betaチームはグループBを6-1で一位通過しました。alpha,betaともに1回戦は勝利しましたが、どちらも準決勝で敗退しました。",
-        collapseNum:"four"
+        collapseNum:"five"
     },
     {
         game:"CR",
@@ -51,7 +51,7 @@ var achievements = [
         rank:"3rd",
         descriptionEN:"10 of the top teams in APAC competed in the 3rd season of GKR Leagues. The league featured a double round-robin format with each team playing a total of 18 matches. Rising Sun Esports had a rough first half with a 5-4 record. However, Rising Sun Esports bounced back and only lost 2 matches in the second half, going 12-6 and finishing 4th place in the regular season. Top 5 teams advanced to the playoffs, where Rising Sun Esports was able to defeat Team CMC and Ako Roshi to claim the 3rd place spot.",
         descriptionJP:"GKR Leagues Season 3には、APAC地域のトップチーム10チームが参加しました。リーグは総当たり戦が二回行われ、各チームは合計18試合を戦いました。Rising Sun Esportsは、前半は5-4と苦戦を強いられました。後半戦では2試合に敗れただけで、12勝6敗でレギュラーシーズン4位通過となりました。Rising SunはTeam CMCとAko Roshiを破り、3位に入賞しました。",
-        collapseNum:"five"
+        collapseNum:"four"
     },
     {
         game:"CR",
@@ -60,7 +60,7 @@ var achievements = [
         rank:"1st",
         descriptionEN:"Hosted by LooT, the World Cup featured 52 countries. Rising Sun Esports became the representative of Japan after defeating Takeshi Japan 2-0. Rising Sun Esports went 4-2 and finished 2nd in the Group Qualifiers, advancing to the Group Stages. Rising Sun Esports faced Austria, Brazil, and Venezuela in the Group Stages, and advanced to the Finals bracket with a 2-1 record. The Finals tournament featured 8 countries - Australia, Hong Kong, Austria, Greece, Romania, Poland, Singapore, and Japan. Rising Sun Esports beat Singapore in the quarter-finals 3-1, Romania in the semi-finals 3-1, and Australia in the Finals 3-0. Overall, the team went 10-2 to become the World Champions!",
         descriptionJP:"LooT主催のワールドカップは世界各地から52カ国が参加。Rising Sun Esportsは、TAKESHI Japan を2-0で破り、日本代表となりました。4勝2敗でグループ予選を2位で通過し、グループステージに進出しました。グループステージではオーストリア、ブラジル、ベネズエラと対戦し、2勝1敗で決勝トーナメント進出を果たしました。決勝トーナメントには、オーストラリア、香港、オーストリア、ギリシャ、ルーマニア、ポーランド、シンガポール、日本の8カ国が出場しました。準々決勝でシンガポールを3-1、準決勝でルーマニアを3-1、決勝でオーストラリアを3-0で下しました。グループ予選からの成績を合わせると、Rising Sun Esportsは10勝2敗で世界チャンピオンとなりました。",
-        collapseNum:"six"
+        collapseNum:"three"
     },
     {
         game:"CR",
@@ -69,7 +69,7 @@ var achievements = [
         rank:"2nd",
         descriptionEN:"The inaugural season of Neo Clash League consisted of 10 teams. Each team played 6 matches in the regular season where Rising Sun Esports placed first with a 5-1 record. For the playoffs, 6 teams advanced and the top 2 teams received a first-round bye. In the semi-finals, Rising Sun Esports defeated Team Phoenix 3-1 to advance to the finals. In the finals, Rising Sun Esports fought hard but lost to Intellectuals 3-4, ending in 2nd place.",
         descriptionJP:"Neo Clash League Season 1は10チームのリーグ戦でした。レギュラーシーズンは各チーム6試合が行われ、Rising Sun Esportsは5勝1敗で首位に立ちました。準決勝では、Team Phoenixを3-1で破り、決勝戦に進出しました。決勝戦では奮闘の末、Intellectualsに3-4で敗れ、準優勝となりました。",
-        collapseNum:"seven"
+        collapseNum:"two"
     },
     {
         game:"CR",
@@ -78,7 +78,7 @@ var achievements = [
         rank:"1st",
         descriptionEN:"The group stages of Clash Star league included 10 teams in each group. Rising Sun Esports was placed in Group C which included some of the top EU teams. A round-robin was played with every team in the group, and Rising Sun Esports dominated the group going undefeated and placing 1st place.",
         descriptionJP:"Clash Star Leagueのグループステージには、各グループ10チームが参加。Rising Sun Esportsは、EUのトップチームが参加するグループCに入りました。グループ内の全チームとのラウンドロビンが行われ、Rising Sun Esportsが無敗で1位を獲得しました。",
-        collapseNum:"eight"
+        collapseNum:"one"
     }    
 ]
 
