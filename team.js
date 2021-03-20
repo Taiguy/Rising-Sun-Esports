2c200d77347a8f8db746dac21a5f1e9a8cf83f7a
Shota <shota1wa@yahoo.co.jp>
2021-03-19 17:02:54 -0700

    Removed PS and added RSB

--- team.js
+++ team.js
@@ -138,158 +138,36 @@ for(i = 0; i < apac.length; i++){
 }
 
 
-
-// PastSinner --------------------------------------------
-var pastSinner = [
-    {
-        flag:"",
-        name:"팝콘TVBJ한상일",
-        twitter:"https://twitter.com/MENTAL_PSR",
-        link:"https://royaleapi.com/player/800YLPGYU"
-    },
-    {
-        flag:"",
-        name:"KAIST",
-        twitter:"https://twitter.com/KAISTCR",
-        link:"https://royaleapi.com/player/2298CLV2R"
-    },
-    {
-        flag:"",
-        name:"TOÝ",
-        twitter:"https://twitter.com/toy_68",
-        link:"https://royaleapi.com/player/P8RPLU08"
-    },
-    {
-        flag:"",
-        name:"PIVOT",
-        twitter:"https://twitter.com/YoungheonL",
-        link:"https://royaleapi.com/player/9QJVR20G"
-    },
-    {
-        flag:"",
-        name:"Munch",
-        twitter:"https://twitter.com/munchcr",
-        link:"https://royaleapi.com/player/82PY20YY"
-    },
-    {
-        flag:"",
-        name:"JESTER€Ro(Ya)Ri",
-        twitter:"https://twitter.com/JESTER_RoYaRi",
-        link:"https://royaleapi.com/player/8L8CC2VJ2"
-    },
-    {
-        flag:"",
-        name:"Greed",
-        twitter:"https://twitter.com/Greed_crl",
-        link:"https://royaleapi.com/player/2PQJYJV80"
-    },
-    {
-        flag:"",
-        name:"김재투",
-        twitter:"https://twitter.com/jaetwokim",
-        link:"https://royaleapi.com/player/RUQGRYUJ"
-    },
-    {
-        flag:"",
-        name:"최소무덤장인",
-        twitter:"https://twitter.com/xIzqZW8srr2T5sy",
-        link:"https://royaleapi.com/player/PRQLUGJU"
-    },
-    {
-        flag:"",
-        name:"Delta Kor",
-        twitter:"https://twitter.com/dQcOBbUsZAZlDIt",
-        link:"https://royaleapi.com/player/2GQJGLCQP"
-    },
-    {
-        flag:"",
-        name:"엘티노",
-        twitter:"https://twitter.com/junhyeok_2003",
-        link:"https://royaleapi.com/player/2GR0JUL0V"
-    },
-    {
-        flag:"",
-        name:"Ra sena",
-        twitter:"https://twitter.com/sena040817",
-        link:"https://royaleapi.com/player/90CRLJP"
-    },
-    {
-        flag:"",
-        name:"KÄİ",
-        twitter:"https://twitter.com/Kai15911613",
-        link:"https://royaleapi.com/player/Y9J809JR"
-    },
-    {
-        flag:"",
-        name:"Wimpy Kid",
-        twitter:"https://twitter.com/ytwimpykidcr",
-        link:"https://royaleapi.com/player/YVGQVC9Y"
-    },
-    {
-        flag:"",
-        name:"RoThy",
-        twitter:"https://twitter.com/XCB6ADK93x4vUkF",
-        link:"https://royaleapi.com/player/JPRRUQVQ"
-    },
-    {
-        flag:"",
-        name:"ice wizard",
-        twitter:"https://twitter.com/nonno950917",
-        link:"https://royaleapi.com/player/20P0VLJL8"
-    },
-    {
-        flag:"",
-        name:"N A V E R",
-        twitter:"https://twitter.com/naver_cr",
-        link:"https://royaleapi.com/player/2GL2PCGGY"
-    },
-    {
-        flag:"",
-        name:"즹앙이",
-        twitter:"https://twitter.com/Lu4suCqy5PZx8ZU",
-        link:"https://royaleapi.com/player/PUG99PUJ"
-    },
-    {
-        flag:"",
-        name:"아쿠아",
-        twitter:"https://twitter.com/omichrom",
-        link:"https://royaleapi.com/player/9VGUL0PR"
-    }
-]
-
-pastSinner = pastSinner.sort(nameSort);
-for(i = 0; i < pastSinner.length; i++){
-    $('.pastSinner').append(
-        '<div class="player">' +
-            '<div class="name"><h6>'+ (i+1) +'. '+pastSinner[i].name +'</h6></div>'+
-            '<div class="icon twitter"><a href="'+ pastSinner[i].twitter +'"><img src="twitter.png" class="stats-icon"></img></a></div>'+
-            '<div class="icon api"><a href="'+ pastSinner[i].link +'"><img src="royaleapi-logo.png" class="stats-icon"></a></div>'+
-        '</div>')
-}
-
-
-
 // Wild Rift  --------------------------------------------
 
 var RSB = [
     {
         flag:"",
-        name:"Coming Soon...",
-        twitter:"",
+        name:"Tori",
+        twitter:"https://twitter.com/Fw_tori3",
         
     },
-    // {
-    //     flag:"",
-    //     name:"",
-    //     twitter:"",
+    {
+        flag:"",
+        name:"Dragon",
+        twitter:"",
        
-    // },
-    // {
-    //     flag:"",
-    //     name:"",
-    //     twitter:"",
-        
-    // },
+    },
+    {
+        flag:"",
+        name:"Magic",
+        twitter:"https://twitter.com/Magic_0119",
+    },
+    {
+        flag:"",
+        name:"Paseri",
+        twitter:"https://twitter.com/wildrift3150",
+    },
+    {
+        flag:"",
+        name:"SugarDog",
+        twitter:"https://twitter.com/satoh_inu",
+    },
 ]
 
 RSB = RSB.sort(nameSort);
